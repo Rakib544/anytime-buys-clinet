5f7a9e31bd0c77651ce7e078bf18c5b42d93512d
Rakib544 <md.rakib10122003@gmail.com>
2021-03-30 23:00:02 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Admin from './components/Admin/Admin';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admin = () => {
+    return (
+        <div>
+            <h3>This is admin page</h3>
+        </div>
+    );
+};
+
+export default Admin;
